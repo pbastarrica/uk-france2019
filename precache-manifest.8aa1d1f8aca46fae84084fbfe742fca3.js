a63de6e8546e6d31395fbe44c13b13a3712d4b67
Pedro <pedrobastarrica@gmail.com>
2018-11-18 20:42:50 -0300

    Updates

---
+++ precache-manifest.8aa1d1f8aca46fae84084fbfe742fca3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f33ffa823e05cf951b2",
+    "url": "/uk-france2019/static/css/main.83303adf.chunk.css"
+  },
+  {
+    "revision": "6f33ffa823e05cf951b2",
+    "url": "/uk-france2019/static/js/main.6f33ffa8.chunk.js"
+  },
+  {
+    "revision": "3f956256f4a3ae2bb4a1",
+    "url": "/uk-france2019/static/js/1.3f956256.chunk.js"
+  },
+  {
+    "revision": "28192b7274d407ce25c5",
+    "url": "/uk-france2019/static/js/runtime~main.28192b72.js"
+  },
+  {
+    "revision": "10d9b4b579079adfd43465d71b43538a",
+    "url": "/uk-france2019/static/media/image1.10d9b4b5.jpg"
+  },
+  {
+    "revision": "99792ff3f5ae4271d2cfc91af9ec203c",
+    "url": "/uk-france2019/static/media/image3.99792ff3.jpg"
+  },
+  {
+    "revision": "cf04da4b9b82ea828c7880344aa5208e",
+    "url": "/uk-france2019/static/media/image2.cf04da4b.jpg"
+  },
+  {
+    "revision": "3092f939b2cd385168c18c8e6d3dfd37",
+    "url": "/uk-france2019/index.html"
+  }
+];
